fee01d7e365d1b42a0c28ef0e9579e68be7615ee
Mouslih <ismailmouslih23@gmail.com>
2025-09-24 21:36:42 +0100

    default prevention form and finnaly vercel analytics solved

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { BrowserRouter,Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import Notfound from './pages/Notfound'
-import { Analytics } from "@vercel/analytics/next"
 
 
 function App() {
@@ -14,7 +13,6 @@ function App() {
       <Route index element={<Home/>}/>
       <Route path='*' element={<Notfound/>}/>
 
-    <Analytics/>
     </Routes>
     </BrowserRouter>
     
